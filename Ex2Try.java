9d0af9bd4d45158cc5cf78a2ab392f5e3a4601b3
EndreusSre <endreus.valerio@gmail.com>
2025-07-23 22:09:04 -0300

    Update Ex2Try.java

--- Ex2Try.java
+++ Ex2Try.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 public class Ex2Try {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
-        System.out.println("Digite um numero x: ");
-        int x = entrada.nextInt();
-        
-        System.out.println("Digite um numero y: ");
-        int y = entrada.nextInt();
 
         try {
+            System.out.println("Digite um numero x: ");
+            int x = entrada.nextInt();
+
+            System.out.println("Digite um numero y: ");
+            int y = entrada.nextInt();
             int res = x + y;
             System.out.println("resultado da sua soma : " + res);
-        }catch(InputMismatchException e) {
-            e.getMessage();
+        } catch (InputMismatchException e) {
+            System.err.println("ERRO " + e.getMessage());
             e.printStackTrace();
         }
 
