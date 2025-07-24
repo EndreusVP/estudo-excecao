f5067b7a11a1c7da9af506048dd5b332b887808c
EndreusSre <endreus.valerio@gmail.com>
2025-07-23 21:39:02 -0300

    Update Ex1Try.java

--- Ex1Try.java
+++ Ex1Try.java
@@ -3,27 +3,22 @@ import java.util.Scanner;
 public class Ex1Try {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        
+
         System.out.println("Digite um numero x: ");
         int x = entrada.nextInt();
 
         System.out.println("Digite um numero y: ");
         int y = entrada.nextInt();
 
-        if(y == 0) {
-            try {
-                int res = x / y;
-                System.out.println("resultado da divisão : " + res);
-            } catch (ArithmeticException e) {
-                System.err.println("ERRO: NÃO É POSSIVEL FAZER DIVISÃO POR " + y);
-            } finally {
-                System.out.println("Tente novamente...");
-            }
-        } else {
+        try {
             int res = x / y;
             System.out.println("resultado da divisão : " + res);
+        } catch (ArithmeticException e) {
+            System.err.println("ERRO: NÃO É POSSIVEL FAZER DIVISÃO POR " + y);
+        } finally {
+            System.out.println("Tente novamente...");
         }
-
+        
         entrada.close();
 
     }
