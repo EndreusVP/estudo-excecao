b300aae990658c6c4b04dec488b7d3a9a1217e8c
EndreusSre <endreus.valerio@gmail.com>
2025-07-23 22:02:40 -0300

    commit

--- Ex2Try.java
+++ Ex2Try.java
@@ -1,25 +1,24 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Ex1Try {
+public class Ex2Try {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-
+        
         System.out.println("Digite um numero x: ");
         int x = entrada.nextInt();
-
+        
         System.out.println("Digite um numero y: ");
         int y = entrada.nextInt();
 
         try {
-            int res = x / y;
-            System.out.println("resultado da divisão : " + res);
-        } catch (ArithmeticException e) {
-            System.err.println("ERRO: NÃO É POSSIVEL FAZER DIVISÃO POR " + y);
-        } finally {
-            System.out.println("OBRIGADO POR RESPONDER");
+            int res = x + y;
+            System.out.println("resultado da sua soma : " + res);
+        }catch(InputMismatchException e) {
+            e.getMessage();
+            e.printStackTrace();
         }
 
         entrada.close();
-
     }
 }
